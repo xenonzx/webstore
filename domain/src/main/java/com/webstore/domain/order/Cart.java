6127b5d4219eb8d1a98369a568e30dc1b160659b
eldakhakhny <ahmed.eldakhakhny@blink22.com>
2022-03-29 13:39:04 +0200

    allowing cart object to be created with arraylist of line items or with an empty list of line items

--- domain/src/main/java/com/webstore/domain/order/Cart.java
+++ domain/src/main/java/com/webstore/domain/order/Cart.java
@@ -3,15 +3,20 @@ package com.webstore.domain.order;
 import com.webstore.domain.storelisting.Product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.util.ArrayList;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
     @Getter
-    private final ArrayList<LineItem> items;
+    @NonNull
+    private ArrayList<LineItem> items = new ArrayList<>();
+
     void addLineItem(Product product, int quantity) {
-        items.stream().forEach(lineItem -> {
+        items.forEach(lineItem -> {
             if (lineItem.getProductId() == product.getSkuNumber()) {
                 lineItem.addQuantity(quantity);
             }
