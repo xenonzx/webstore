4f80b9a1054f7288a28be60a6a9dccd2f9505573
eldakhakhny <ahmed.eldakhakhny@blink22.com>
2022-03-29 12:03:29 +0200

    application layer now depend on domain layer

--- src/main/java/com/megastores/webstore/WebStoreApplication.java
+++ src/main/java/com/megastores/webstore/WebStoreApplication.java
@@ -1,18 +1,24 @@
 package com.megastores.webstore;
 
+import com.webstore.domain.order.Cart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class WebstoreApplication {
+@RestController
+public class WebStoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebstoreApplication.class, args);
+        SpringApplication.run(WebStoreApplication.class, args);
     }
 
     @GetMapping("/v1/orders")
     public String getOrder() {
         return "in progress";
+    }
+    Cart getDummyCart(){
+
     }
 }
