fe3f01cefb4d4e5b0f0d63d9c6b1caaf27a8adfd
eldakhakhny <ahmed.eldakhakhny@blink22.com>
2022-04-10 12:53:24 +0200

    using jdbc auth

--- src/main/java/com/megastores/webstore/security/SecurityConfigs.java
+++ src/main/java/com/megastores/webstore/security/SecurityConfigs.java
@@ -1,5 +1,6 @@
 package com.megastores.webstore.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,14 +9,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 
 @EnableWebSecurity
 public class SecurityConfigs extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
+        auth.jdbcAuthentication().dataSource(dataSource)
                 .withUser("admin").password("1234").roles(Admin.ROLE_NAME).and()
                 .withUser("user").password("qwerty").roles(User.ROLE_NAME);
+        //  auth.inMemoryAuthentication()
+
     }
 
     @Override
